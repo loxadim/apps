1c6c920e1f8bb888f8fe97bccaff5ac45d27a6ac
Luke Schoen <ltfschoen@gmail.com>
2018-09-27 13:46:34 +1000

    refactor: Move exports to bottom of defaults.ts file

--- packages/ui-keyring/src/defaults.ts
+++ packages/ui-keyring/src/defaults.ts
@@ -7,7 +7,7 @@ import { decodeAddress } from '@polkadot/keyring';
 
 const ACCOUNT_PREFIX = 'account:';
 const ADDRESS_PREFIX = 'address:';
-export const MAX_PASS_LEN = 32;
+const MAX_PASS_LEN = 32;
 
 function toHex (address: string): string {
   return u8aToHex(
@@ -15,12 +15,20 @@ function toHex (address: string): string {
   );
 }
 
-export const accountKey = (address: string): string =>
-  `${ACCOUNT_PREFIX}${toHex(address)}`;
+const accountKey = (address: string): string =>
+  `${ACCOUNT_PREFIX}${address}`;
 
-export const addressKey = (address: string): string =>
-  `${ADDRESS_PREFIX}${toHex(address)}`;
+const addressKey = (address: string): string =>
+  `${ADDRESS_PREFIX}${address}`;
 
-export const accountRegex = new RegExp(`^${ACCOUNT_PREFIX}`, '');
+const accountRegex = new RegExp(`^${ACCOUNT_PREFIX}`, '');
 
-export const addressRegex = new RegExp(`^${ADDRESS_PREFIX}`, '');
+const addressRegex = new RegExp(`^${ADDRESS_PREFIX}`, '');
+
+export {
+  accountKey,
+  accountRegex,
+  addressKey,
+  addressRegex,
+  MAX_PASS_LEN
+};
